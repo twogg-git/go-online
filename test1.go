8b4405c253d564f125ead060668925e6495cf59f
Catherin Cruz <cruz.catherin@gmail.com>
2017-07-04 17:57:18 -0500

    initial test, helloworld
    initial test, helloworld

---
+++ test1.go
@@ -0,0 +1,16 @@
+FROM golang
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Steps to Hello world!")
+
+	fmt.Println("1 $ go run hello-world.go")
+
+	fmt.Println("2 $ go build hello-world.go")
+
+	fmt.Println("3 $ go run./hello-world")
+
+}
